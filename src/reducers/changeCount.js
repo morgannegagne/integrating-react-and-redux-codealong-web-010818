59fc3ee9ed14f43cb07573f3161b5f4ba672fcce
morgannegagne <morgannegagne@gmail.com>
2018-03-29 13:37:13 -0400

    Done.

---
+++ src/reducers/changeCount.js
@@ -0,0 +1,12 @@
+function changeCount(state = {
+  count: 0,
+}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+    default:
+      return state;
+  };
+};
+
+export default changeCount;
